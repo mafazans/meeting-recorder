bf46f72e88fca867d6fe5baf98f709ce4d191e17
arifmafazans <arif.mafazan@evermos.com>
2026-08-05 21:02:35 +0700

    defer xctest: remove placeholder test, xcode not installed

--- Tests/MeetingRecorderCoreTests/PlaceholderTests.swift
+++
@@ -1,8 +0,0 @@
-import XCTest
-@testable import MeetingRecorderCore
-
-final class PlaceholderTests: XCTestCase {
-    func test_placeholderMessage() {
-        XCTAssertEqual(Placeholder.message, "MeetingRecorderCore scaffold OK")
-    }
-}
